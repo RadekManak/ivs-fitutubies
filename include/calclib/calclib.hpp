464dbae7c4ca70866653c7554e225036b8cd4e50
Radek Maňák <Radek.Manak@protonmail.com>
2020-03-18 19:53:43 +0100

    Implement solver for addition

--- include/calclib/calclib.hpp
+++ include/calclib/calclib.hpp
@@ -1,25 +1,27 @@
 #pragma once
 
-#include <cmath>
+#include <iostream>
+#include <lib/lexertk/lexertk.hpp>
+
+using Token = lexertk::generator::token_t;
+using Token_type = Token::token_type;
 
 class calcLib {
 
 public:
-
-    static constexpr double PI = M_PI;
-    static constexpr double E = M_E;
-
     static double add(double lhs, double rhs);
     static double sub(double lhs, double rhs);
     static double mul(double lhs, double rhs);
     static double div(double lhs, double rhs);
     static double mod(double lhs, double rhs);
-    static double sin(double num);
-    static double cos(double num);
-    static double tan(double num);
-    static double log(double num);
-    static double ln(double num);
+    static double sin(double n);
+    static double cos(double n);
+    static double tan(double n);
     static double pow(double base, int exponent);
-    static double factorial(double num);
+    static double factorial(double n);
+    static double solveEquation(const std::string &expression);
+
+protected:
+    static int parseEquation(const std::string &expression, std::vector<lexertk::generator::token_t> &outTokens);
 
 };
--- src/calclib.cpp
+++ src/calclib.cpp
@@ -1,5 +1,52 @@
+#include <vector>
 #include "calclib/calclib.hpp"
 
 double calcLib::add(double a, double b) {
     return a + b;
+}
+
+int calcLib::parseEquation(const std::string &expression, std::vector<lexertk::generator::token_t> &outTokens){
+    lexertk::generator generator;
+
+    if (!generator.process(expression))
+    {
+        std::cout << "Failed to lex: " << expression << std::endl;
+        return 1;
+    }
+
+    lexertk::helper::bracket_checker bc;
+    bc.process(generator);
+
+    if (!bc.result())
+    {
+        std::cout << "Failed Bracket Checker!" << std::endl;
+        return 1;
+    }
+
+    lexertk::helper::commutative_inserter ci;
+    ci.process(generator);
+
+    while(!generator.finished()){
+        outTokens.emplace_back(generator.next_token());
+    }
+
+    return 0;
+}
+
+double calcLib::solveEquation(const std::string &expression) {
+    std::vector<Token> tokens;
+    calcLib::parseEquation(expression, tokens);
+    while(true){
+        auto token = std::find_if(tokens.begin(), tokens.end(), [&](const Token &token){return token.type == Token_type::e_add;});
+        if (token == tokens.end()){break;}
+
+        auto previous = token-1;
+        auto next = token+1;
+        if (previous->type == Token_type::e_number && next->type == Token_type::e_number){
+            previous->value = std::to_string(std::stod(previous->value) + std::stod(next->value));
+            tokens.erase(token);
+            tokens.erase(token);
+        }
+    }
+    return std::stod(tokens[0].value);
 }
