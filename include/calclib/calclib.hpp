8b363c3e130d6caf8a574ddaff68031349c1051d
Radek Maňák <Radek.Manak@protonmail.com>
2020-03-18 20:07:43 +0100

    Declare calclib header functions

--- include/calclib/calclib.hpp
+++ include/calclib/calclib.hpp
@@ -1,7 +1,25 @@
 #pragma once
 
-namespace calcLib {
+#include <cmath>
 
-    double add(double a, double b);
+class calcLib {
+
+public:
+
+    static constexpr double PI = M_PI;
+    static constexpr double E = M_E;
+
+    static double add(double lhs, double rhs);
+    static double sub(double lhs, double rhs);
+    static double mul(double lhs, double rhs);
+    static double div(double lhs, double rhs);
+    static double mod(double lhs, double rhs);
+    static double sin(double num);
+    static double cos(double num);
+    static double tan(double num);
+    static double log(double num);
+    static double ln(double num);
+    static double pow(double base, int exponent);
+    static double factorial(double num);
 
 };
