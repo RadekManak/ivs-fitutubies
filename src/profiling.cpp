b2efd06e2de8f6878f8e76663e286330ff8007ab
Radek Maňák <Radek.Manak@protonmail.com>
2020-04-10 15:53:33 +0200

    Fix tests and profiling to work with new library header

--- src/profiling.cpp
+++ src/profiling.cpp
@@ -2,7 +2,8 @@
 #include "calclib/calclib.hpp"
 
 int main() {
-	std::cout << "Sample profiling source code " << calcLib::add(1,2) << "\n";
+    calcLib calc;
+	std::cout << "Sample profiling source code " << calc.solveEquation("1+2") << "\n";
 	return 0;
 }
 
--- src/test/calclib_test.cpp
+++ src/test/calclib_test.cpp
@@ -2,54 +2,39 @@
 #include "gtest/gtest.h"
 
 using namespace ::testing;
-using namespace calcLib;
+calcLib calc{calcLib::ResultFormat::fixed, 8};
+
+TEST(CalcLibTest, numbers) {
+    EXPECT_EQ(calc.solveEquation("0"), "0.00000000");
+    EXPECT_EQ(calc.solveEquation("0.2"), "0.20000000");
+    EXPECT_EQ(calc.solveEquation("1"), "1.00000000");
+    EXPECT_EQ(calc.solveEquation("-1"), "-1.00000000");
+}
 
 TEST(CalcLibTest, addition) {
-    EXPECT_EQ(add(3,2), 5);
-    EXPECT_EQ(add(-3,2), -1);
-    EXPECT_EQ(add(0,12), 12);
-    EXPECT_EQ(add(0,0), 0);
-    EXPECT_EQ(add(-12345678, 12345679), 1);
-    EXPECT_EQ(add(3.5, 2.4), 5.9);
-    EXPECT_EQ(add(-3.1, 2.1), -1);
-    EXPECT_EQ(add(0, -12), -12);
-    EXPECT_EQ(add(12345678.9, -12345678.4), 0.5);
+    EXPECT_EQ(calc.solveEquation("15+3"), "18.00000000");
+    EXPECT_EQ(calc.solveEquation("15+-3"), "12.00000000");
 }
 
 TEST(CalcLibTest, subtraction) {
-    EXPECT_EQ(sub(3,2), 1);
-    EXPECT_EQ(sub(-3,2), -5);
-    EXPECT_EQ(sub(0,12), -12);
-    EXPECT_EQ(sub(0,0), 0);
-    EXPECT_EQ(sub(-12345678, 12345679), -24691357);
-    EXPECT_EQ(sub(3.5, 2.4), 1.1);
-    EXPECT_EQ(sub(-3.1, 2.1), (-5.2));
-    EXPECT_EQ(sub(0, -12), 12);
-    EXPECT_EQ(sub(1239.9, -1234.1), 2474);
+    EXPECT_EQ(calc.solveEquation("10.2-3"), "7.20000000");
 }
 
 TEST(CalcLibTest, multiplication) {
-    EXPECT_EQ(mul(3,2), 6);
-    EXPECT_EQ(mul(-3,2), -6);
-    EXPECT_EQ(mul(0,12), 0);
-    EXPECT_EQ(mul(0,0), 0);
-    EXPECT_EQ(mul(-12345678, 12), -148148136);
-    EXPECT_EQ(mul(3.5, 2.4), 8.4);
-    EXPECT_DOUBLE_EQ(mul(-3.1, 2.1), -6.51);
-    EXPECT_EQ(mul(4, -12), -48);
-    EXPECT_DOUBLE_EQ(mul(1239.9, -13.1), -16242.69);
+    EXPECT_EQ(calc.solveEquation("10*3"), "30.00000000");
+    EXPECT_EQ(calc.solveEquation("10*+3"), "30.00000000");
+    EXPECT_EQ(calc.solveEquation("10*-3"), "-30.00000000");
 }
 
 TEST(CalcLibTest, division) {
-    EXPECT_DOUBLE_EQ(calcLib::div(3,2), 1.5);
+    EXPECT_EQ(calc.solveEquation("10/3"), "3.33333333");
+    EXPECT_EQ(calc.solveEquation("10/3/2"), "1.66666667");
+    EXPECT_EQ(calc.solveEquation("10/0"), "Division by zero");
 }
 
-
-    /*EXPECT_DOUBLE_EQ(div(-3,2), -1.5);
-    EXPECT_EQ(div(0,12), 0);
-    EXPECT_ANY_THROW(div(3,0));
-    EXPECT_DOUBLE_EQ(div(-12345678, 12), -1028806.5);
-    EXPECT_DOUBLE_EQ(div(3.5, 2.4), 1.458333333);
-    EXPECT_DOUBLE_EQ(div(-3.1, 2.1), -1.476190476);
-    EXPECT_DOUBLE_EQ(div(4, -12), -0.333333333);
-    EXPECT_DOUBLE_EQ(div(1239.9, -13.1), -94.648854962);*/
+TEST(CalcLibTest, Precedence) {
+    EXPECT_EQ(calc.solveEquation("10+3*7"), "31.00000000");
+    EXPECT_EQ(calc.solveEquation("3*7+10"), "31.00000000");
+    EXPECT_EQ(calc.solveEquation("2*5/2"), "5.00000000");
+    //EXPECT_EQ(calc.solveEquation("7/3*3"), "7.00000000");
+}
