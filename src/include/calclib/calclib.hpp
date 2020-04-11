a6602aa0f135bce869e702aefb626f04fb7a07a7
Radek Maňák <Radek.Manak@protonmail.com>
2020-04-11 12:27:03 +0200

    Refactor calcLib

--- src/include/calclib/calclib.hpp
+++ src/include/calclib/calclib.hpp
@@ -8,7 +8,7 @@
 using Token_type = lexertk::token::token_type;
 
 /**
- * Custom token holding value in double for Token_type::e_number and in std::string for everything else.
+ * Custom token. Storing value in double for Token_type::e_number and in std::string for everything else.
  */
 class Token{
 public:
@@ -31,28 +31,28 @@ public:
     int precision;
 private:
     std::map<std::string, double> variables;
-    double add(double lhs, double rhs);
-    double sub(double lhs, double rhs);
-    double mul(double lhs, double rhs);
-    double div(double lhs, double rhs);
-    double mod(double lhs, double rhs);
-    double sin(double n);
-    double cos(double n);
-    double tan(double n);
-    double sqrt(double n);
-    double pow(double base, double exponent);
-    double root(double degree, double num);
-    double log(double base, double num);
-    double log(double num);
-    double factorial(double n);
+    static double add(double lhs, double rhs);
+    static double sub(double lhs, double rhs);
+    static double mul(double lhs, double rhs);
+    static double div(double lhs, double rhs);
+    static double mod(double lhs, double rhs);
+    static double sin(double n);
+    static double cos(double n);
+    static double tan(double n);
+    static double sqrt(double n);
+    static double pow(double base, double exponent);
+    static double root(double degree, double num);
+    static double log(double base, double num);
+    static double log(double num);
+    static double factorial(double n);
     std::string formatResult(double result);
-    int parseEquation(const std::string &expression, std::vector<Token> &outTokens);
-    double calculateBinaryOperation(double lhs, double rhs, const Token& operation);
-    double calculateUnaryOperation(double num, const Token& operation);
-    double calculateFunction(const std::vector<Token> &parameters, const Token &function);
-    void solveBinaryOperation(std::vector<Token> &tokens, const Token& operation, bool reverse);
-    void solveUnaryPlusMinus(std::vector<Token> &tokens);
-    void solveRightAssociativeUnary(std::vector<Token> &vector, const Token &operation);
-    void solveFunctions(std::vector<Token> &tokens);
-    void solveConstants(std::vector<Token> &tokens, const Token &constant);
+    static int parseEquation(const std::string &expression, std::vector<Token> &outTokens);
+    static double calculateBinaryOperation(double lhs, double rhs, const Token& operation);
+    static double calculateUnaryOperation(double num, const Token& operation);
+    static double calculateFunction(const std::vector<Token> &parameters, const Token &function);
+    static void solveBinaryOperation(std::vector<Token> &tokens, const Token& operation, bool reverse);
+    static void solveUnaryPlusMinus(std::vector<Token> &tokens);
+    static void solveRightAssociativeUnary(std::vector<Token> &vector, const Token &operation);
+    static void solveFunctions(std::vector<Token> &tokens);
+    void solveVariable(std::vector<Token> &tokens, const Token &variable);
 };
