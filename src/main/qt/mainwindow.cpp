b9a46d23e557cebf28b70c49ae7f6f2dce4f8df9
Radek Maňák <Radek.Manak@protonmail.com>
2020-04-29 12:53:56 +0200

    Add application icon
    Icon created by: Sabína Gulčíková

--- src/main/qt/mainwindow.cpp
+++ src/main/qt/mainwindow.cpp
@@ -11,6 +11,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowIcon(QIcon(":/fitutubies-calculator.png"));
     readSettings();
 
     // Menubar
