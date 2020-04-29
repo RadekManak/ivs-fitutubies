2d742bd6cea8376b60c6c8b56570ac40424a6178
Radek Maňák <Radek.Manak@protonmail.com>
2020-04-29 17:27:37 +0200

    Remove temporary and example files

--- debian/fitutubiescalculator.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the fitutubiescalculator package
-#
-0 4	* * *	root	[ -x /usr/bin/fitutubiescalculator_maintenance ] && /usr/bin/fitutubiescalculator_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(fitutubiescalculator):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="fitutubiescalculator" command="/usr/bin/fitutubiescalculator"
